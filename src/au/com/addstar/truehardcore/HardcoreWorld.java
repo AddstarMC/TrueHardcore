d2432eefe0fbbc71f8990815edb636544892bacf
addstar <adam@addstar.com.au>
2013-09-18 20:42:05 +1000

    Forgot to add HardcoreWorld class!

---
+++ src/au/com/addstar/truehardcore/HardcoreWorld.java
@@ -0,0 +1,54 @@
+package au.com.addstar.truehardcore;
+
+import org.bukkit.World;
+
+public class HardcoreWorld {
+	public HardcoreWorld() {
+		// Nothing needed here yet
+	}
+	
+	private World world;
+	private String greeting;
+	private Integer bantime;
+	private Integer spawndistance;
+	private Integer spawnprotection;
+
+	public World getWorld() {
+		return world;
+	}
+	public void setWorld(World world) {
+		this.world = world;
+	}
+
+	public String getGreeting() {
+		return greeting;
+	}
+
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
+	}
+
+	public Integer getBantime() {
+		return bantime;
+	}
+
+	public void setBantime(Integer bantime) {
+		this.bantime = bantime;
+	}
+
+	public Integer getSpawnDistance() {
+		return spawndistance;
+	}
+
+	public void setSpawnDistance(Integer spawndistance) {
+		this.spawndistance = spawndistance;
+	}
+
+	public Integer getSpawnProtection() {
+		return spawnprotection;
+	}
+
+	public void setSpawnProtection(Integer spawnprotection) {
+		this.spawnprotection = spawnprotection;
+	}
+}
